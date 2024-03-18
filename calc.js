4167822155017aaf3519847c27659d833fd75799
Ben Barr <benjamincorybarr@gmail.com>
2024-03-18 16:09:07 -0700

    Add button event listeners and store input

--- calc.js
+++ calc.js
@@ -14,9 +14,7 @@ function divide(num1, num2) {
     return num2 !== 0 ? num1 / num2 : 'Cannot divide by zero';
 }
 
-let beginningNumber = 0;
-let calculationOperator = 'operatorSymbol';
-let secondNumber = 0;
+
 
 function operate(beginningNumber, calculationOperator, secondNumber) {
     switch (calculationOperator) {
@@ -33,4 +31,41 @@ function operate(beginningNumber, calculationOperator, secondNumber) {
     }
 }
 
-console.log(operate(2, '/', 0));
+let beginningNumber = 0;
+let calculationOperator = 'waitingForOperatorSymbol';
+let secondNumber = 0;
+let newNumber = false;
+
+const numberButtons = document.querySelectorAll('#number-buttons button');
+const operatorButtons = document.querySelectorAll('#operator-buttons button');
+const calcDisplay = document.querySelector('#display-value p')
+
+
+numberButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        if (calcDisplay.textContent === '0' || newNumber) {
+            calcDisplay.textContent = this.textContent;
+            newNumber = false;
+        } else {
+            calcDisplay.textContent += this.textContent;
+        }
+        if (calculationOperator === 'waitingForOperatorSymbol' || !calculationOperator) {
+            beginningNumber = parseFloat(calcDisplay.textContent);
+        } else {
+            secondNumber = parseFloat(calcDisplay.textContent);
+        }
+    });
+});
+
+operatorButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        if (calcDisplay.textContent === '0') {
+            calcDisplay.textContent = 0;
+            calculationOperator = 'waitingForOperatorSymbol';
+        } else {
+            calcDisplay.textContent += this.textContent;
+            calculationOperator = this.textContent;
+            newNumber = true;
+        }
+    })
+})
